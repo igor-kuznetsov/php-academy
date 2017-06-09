62e5add08bbddc80393c5b838f4f176d5c61c336
Igor Kuznetsov <webdevastator@gmail.com>
2017-06-09 13:26:48 +0300

    functions - preparation for 3rd lesson

---
+++ functions/practice/7/form.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <title>Form</title>
+</head>
+<body>
+<section>
+    <?php
+    ?>
+</section>
+<section>
+    <form method="post" action="">
+        <textarea name="text"></textarea><br>
+        <p><input type="submit"></p>
+    </form>
+</section>
+</body>
+</html>
